9345e66592d294b38653fe317536258705552ea8
joo-you <yousefhammam010035@gmail.com>
2026-05-17 11:50:43 +0300

    Add files via upload

--- apps.py
+++ apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
 
-class OctopusConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'octopus'
+class ApiConfig(AppConfig):
+    name = 'api'
